0e306e566c01f49c84aefc11daf6082a4e68b953
vuvannghiawork <vuvannghia.work@gmail.com>
2026-07-18 01:10:44 +0700

    auto commit: 2026-07-18 01:10:44

--- python/audio.py
+++ python/audio.py
@@ -3,7 +3,6 @@ import hashlib
 import os
 import re
 import shutil
-import time
 from pathlib import Path
 
 import edge_tts
@@ -384,9 +383,9 @@ if __name__ == "__main__":
     file_path_pdf = r"C:\Users\Admin\Documents\GitHub\docs-slide\latex\main.pdf"
 
     is_open = False
-    # is_open = True
+    is_open = True
 
     if is_open:
         os.startfile(file_path_mp3)
-        time.sleep(2)
+        # time.sleep(2)
         os.startfile(file_path_pdf)
